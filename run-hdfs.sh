8d715f8e96b115875ad483e6b8af83aaacedc3b6
Logic® <48907062+Rezkmike@users.noreply.github.com>
2024-03-30 16:11:43 +0800

    Create run-hdfs.sh

---
+++ run-hdfs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TPL_PATH='./templates'
+HADOOP_PATH='/usr/local/hadoop/etc/hadoop/'
+HADOOP_BIN='/usr/local/hadoop/sbin'
+
+mv -f $TPL_PATH/hdfs-site.conf $HADOOP_PATH/hdfs-site.conf
+mv -f $TPL_PATH/core-site.conf $HADOOP_PATH/core-site.conf
+mv -f $TPL_PATH/mapred-site.conf $HADOOP_PATH/mapred-site.conf
+mv -f $TPL_PATH/yarn-site.conf $HADOOP_PATH/yarn-site.conf
+
+# Test HDFS
+/usr/local/hadoop/bin/hdfs namenode –format
+
+# Run HDFS
+bash $HADOOP_BIN/start-all.sh
